ba0422c96bff7e1734572ff8a2dbbbf9f7ab0e8b
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-10-21 17:50:09 -0400

    Array-Object signInForm

---
+++ ZeroToMastery/JavaScript/Array-Object-signInForm.js
@@ -0,0 +1,62 @@
+let database = [
+  {
+    username: "Frank",
+    password: "soon",
+  },
+  {
+    username: 1234,
+    password: 1234,
+  },
+  {
+    username: "Frank",
+    password: 1234,
+  },
+  {
+    username: 1234,
+    password: "frank",
+  },
+];
+
+let newsfeed = [
+  {
+    username: "Bobby",
+    timeline: "So tired from all that learning!",
+  },
+  {
+    username: "Sally",
+    timeline: "Javascript is sooooo cool!",
+  },
+  {
+    username: "Mitch",
+    timeline: "Javascript is preeetyy cool!",
+  },
+];
+
+let userName = prompt("Enter username");
+let userPW = prompt("Enter password");
+
+function isUserValid(userName, userPW) {
+  for (let i = 0; i < database.length; i++) {
+    if (
+      database[i].username.toString() === userName.toString() &&
+      database[i].password.toString() === userPW.toString()
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function signIn(userName, userPW) {
+  if (isUserValid(userName, userPW)) {
+    console.log(newsfeed);
+  } else {
+    alert("sorry, wrong name & pw");
+    let userName = prompt("username again");
+    let userPW = prompt("password again");
+
+    signIn(userName, userPW);
+  }
+}
+
+signIn(userName, userPW);
