b4066d8ceff2bf39e2cb01a2de2063a5133930f0
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-12-05 17:36:04 -0500

    single-threaded-asynchronouns

---
+++ Interview-Questions/single-threaded-asynchronuns.js
@@ -0,0 +1,12 @@
+// https://samimyaquby.medium.com/how-can-javascript-be-asynchronous-and-single-threaded-at-the-same-time-c13c99bb4703
+// JavaScript be asynchronous and single-threaded at the same time?
+// Event Loop
+
+// Asynchronicity doesn’t necessarily mean multi-threadedness.
+/*
+
+Since these events can happen at unpredictable times 
+and in unpredictable order, 
+we say that the invocation of 
+their handling functions are asynchronous.
+*/
