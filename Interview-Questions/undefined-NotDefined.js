e521b0f618ddcae083d5a624eae6554873e4f250
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-12-09 19:53:34 -0500

    not defined VS undefined

---
+++ Interview-Questions/undefined-NotDefined.js
@@ -0,0 +1,13 @@
+// undefined is like taking some memory
+// Not defined is not even in the memory.
+
+// === HOWEVER, undefined doeson't mean it is empty ===
+// JavaScript is a loosley typed language -> types are flexible
+
+// Never do this
+var a = undefined;
+
+// this is a keyword in JavaScript, it is a bad thing to do in JavaScript
+// what is undefined?
+// it's like a placeholder which is kept inside the variables for memory and it's for a specific purpose
+
