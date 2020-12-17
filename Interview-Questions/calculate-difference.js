5e8e76d7eccf50077e5c98bd9d38e440ddd2e8dd
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-12-16 20:27:43 -0500

    Set & sort & calculate

---
+++ Interview-Questions/calculate-difference.js
@@ -0,0 +1,18 @@
+/*
+    write a function to determine the largest difference
+    between any two numbers in an array of numbers
+*/
+
+let numbers = [12, 2, 6, 5, 5, 6, 9, 10, 10, 33];
+
+let difference = (function (arr){
+    // (1) remove duplicates with Set
+    // pass set back to an array
+    let answer = Array.from(new Set(arr));
+    // (2) Sort the array
+    let sorted = answer.sort((a,b)=> b - a)
+    // (2) largest - smallest = the largest difference 
+    return sorted[0] - sorted[sorted.length - 1]
+})(numbers);
+
+console.log(difference);
