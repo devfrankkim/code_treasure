95940f98640f48a076bafe630f145ed2d02f9a3a
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2021-01-15 10:02:46 -0500

    profile - boolean - check

---
+++ ZeroToMastery/database/profile-boolean.js
@@ -0,0 +1,16 @@
+Boolean([]); // true
+// so how do we want to check against the empty array?
+// simply we say if user.length exists in the case
+applicationCache.get('/profile/:id', (req, res) =>{
+    const {id} = req.params
+    db.select('*').from('users').where({id})
+    .then(user => {
+        if(user.length){
+            res.json(user[0])
+        } else {
+            res.status(400).json('Not found')
+        }
+    })
+    .catch(err => res.status(400).json('error getting user'))
+})
+
