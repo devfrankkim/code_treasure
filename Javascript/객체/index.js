1de9a40bbdd7b371fc37aa0199bb03959aa7b692
heyfranksmile <heyfranksmile@gmail.com>
2022-04-16 21:55:39 -0400

    dot notation vs square bracket

---
+++ Javascript/객체/index.js
@@ -0,0 +1,15 @@
+// 객체 표현 방법
+
+const data = {
+  canadian: {
+    name: "Frank",
+    description: "리액트를 좋아하는 개발자",
+  },
+  korean: {
+    name: "KH",
+    description: "movie 주인공",
+  },
+};
+
+data["canadian"];
+data["korean"];
